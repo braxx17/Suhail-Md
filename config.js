13e8175a4f379769146be9334eedf11a99aae639
braxx17 <110266052+braxx17@users.noreply.github.com>
2024-08-02 05:56:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -55,7 +55,7 @@ global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,256xxxxxxxx";
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
 global.read_status = process.env.AUTO_READ_STATUS || "true"
 global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,256xxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "256754370702,256xxxxxxxx";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
